656a70db7aa55a8f953217c7ecee7df19d4d2414
Simon Adorf <sadorf@nvidia.com>
2026-01-07 10:52:06 -0600

    Import cuml.accel early to avoid import penalty during fit() call.

--- common/src/autogluon/common/__init__.py
+++ common/src/autogluon/common/__init__.py
@@ -1,5 +1,6 @@
 from .dataset import TabularDataset
 from .features.feature_metadata import FeatureMetadata
+from .utils.cuml_accel_utils import is_cuml_accel_available
 from .utils.log_utils import _add_stream_handler
 from .utils.log_utils import fix_logging_if_kaggle as __fix_logging_if_kaggle
 from .version import __version__
@@ -8,3 +9,6 @@ from .version import __version__
 __fix_logging_if_kaggle()
 
 _add_stream_handler()
+
+# Pre-check cuml.accel availability to trigger early import
+is_cuml_accel_available()
